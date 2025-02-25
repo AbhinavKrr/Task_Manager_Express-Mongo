55b8fa262818ca35b3beea81baea767860ce97ba
AbhinavKrr <abhinavkumarblog@gmail.com>
2025-02-25 16:28:30 +0530

    here we go

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -1,13 +1,32 @@
 const Task = require('../models/task');
 
-const getAllTasks = (req, res)=>{
-    res.status(200).json({success: true, data:[]});
+//Get All Tasks
+const getAllTasks = async (req, res)=>{
+    try{
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    }
+    catch(err){
+        res.status(404).json({msg: err});
+    }
 }
 
-const getTask = (req, res)=>{
-    res.status(200).json({success: true, data:[]});
+// Get Single Task
+const getTask = async (req, res)=>{
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOne({_id : taskID});
+        if(!task){
+            return res.status(404).json({msg:"No data Found"});
+        }
+        res.status(200).json({task});
+    }
+    catch(err){
+        res.status(500).json({msg:err})
+    }
 }
 
+// Create a Task
 const createTask = async (req, res)=>{
     try{
         const task = await Task.create(req.body)
@@ -18,12 +37,24 @@ const createTask = async (req, res)=>{
     }
 }
 
+// Update a Task
 const updateTask = (req, res)=>{
     res.status(200).json({success: true, data:[]});
 }
 
-const deleteTask =  (req, res)=>{
-    res.status(200).json({success: true, data:[]});
+// Delete a task
+const deleteTask = async (req, res)=>{
+    try{
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id : taskID});
+        if(!task){
+            return res.status(404).json({msg:"No data Found"});
+        }
+        res.status(200).json({task:task,deleted:true});
+    }
+    catch(err){
+        res.status(500).json({msg:err});
+    }
 }
 
 module.exports = {getAllTasks, getTask, createTask, updateTask, deleteTask}
