1d8eaef79dff268dc08216c5fd20486f70085f61
AbhinavKrr <abhinavkumarblog@gmail.com>
2025-02-27 13:56:31 +0530

    completed

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -1,47 +1,48 @@
 const Task = require('../models/task');
 const asyncWrapper = require('../middleware/asyncWrapper');
+const {createCustomError} = require('../errors/custom-error');
 
 //Get All Tasks
-const getAllTasks = asyncWrapper(async (req, res)=>{
+const getAllTasks = asyncWrapper(async (req, res, next)=>{
         const tasks = await Task.find({});
         res.status(200).json({tasks});
 })
 
 // Get Single Task
-const getTask = asyncWrapper(async (req, res)=>{
+const getTask = asyncWrapper(async (req, res, next)=>{
         const {id:taskID} = req.params;
         const task = await Task.findOne({_id : taskID});
         if(!task){
-            return res.status(404).json({msg:"No data Found"});
+            return next(createCustomError("Not Found", 404));
         }
         res.status(200).json({task});
 })
 
 // Create a Task
-const createTask = asyncWrapper( async (req, res)=>{
+const createTask = asyncWrapper( async (req, res, next)=>{
         const task = await Task.create(req.body)
         res.status(201).json({task});
 })
 
 // Update a Task
-const updateTask = asyncWrapper(async (req, res)=>{
+const updateTask = asyncWrapper(async (req, res, next)=>{
         const {id:taskID} = req.params;
         const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
             new: true, 
             runValidators: true
         });        
         if(!task){
-            return res.status(404).json({msg:"No data Found"});
+            return next(createCustomError("Not Found", 404));
         }
         res.status(200).json({task});
 })
 
 // Delete a task
-const deleteTask = asyncWrapper(async (req, res)=>{
+const deleteTask = asyncWrapper(async (req, res, next)=>{
         const {id:taskID} = req.params;
         const task = await Task.findOneAndDelete({_id : taskID});
         if(!task){
-            return res.status(404).json({msg:"No data Found"});
+            return next(createCustomError("Not Found", 404));
         }
         res.status(200).json({task});
 })
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,10 @@
+const {CustomAPIError} = require('../errors/custom-error')
+
 const errroHandlerMiddleware = (err, req, res, next)=>{
-    return res.status(500).json({err:err});
+    if(err instanceof CustomAPIError){
+        return res.status(err.statusCode).json({msg:err.msg});
+    }
+    return res.status(500).json({ msg: "something went wrong"});
 }
 
 module.exports = errroHandlerMiddleware;
